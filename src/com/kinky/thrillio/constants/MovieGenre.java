3004108538197338852048c7af5536115fe95436
Dave.Graham <kinkyjalepeno@gmail.com>
2019-04-23 11:54:00 +0100

    Initial Commit

---
+++ src/com/kinky/thrillio/constants/MovieGenre.java
@@ -0,0 +1,26 @@
+package com.kinky.thrillio.constants;
+
+public class MovieGenre {
+
+	public MovieGenre() {
+	}
+
+	public static final String CLASSICS = "Classics";
+	public static final String DRAMA = "Drama";
+	public static final String SCIFI_AND_FANTASY = "Sci-Fi & Fantasy";
+	public static final String COMEDY = "Comedy";
+	public static final String CHILDREN_AND_FAMILY = "Children & Family";
+	public static final String ACTION_AND_ADVENTURE = "Action & Adventure";
+	public static final String THRILLER = "Thriller";
+	public static final String MUSIC_AND_MUSICALS = "Music & Musicals";
+	public static final String TELEVISION = "Television";
+	public static final String HORROR = "Horror";
+	public static final String SPECIAL_INTEREST = "Special interest";
+	public static final String INDEPENDENT = "Independent";
+	public static final String SPORTS_AND_FITNESS = "Sports & Fitness";
+	public static final String ANIME_AND_ANIMATION = "Anime and Animation";
+	public static final String GAY_AND_LESBIAN = "Gay & Lesbian";
+	public static final String FAITH_AND_SPIRITUALITY = "Faith & Spirituality";
+	public static final String DOCUMENTARY = "Documentary";
+
+}
